3e42113df540e2dfba70fd44b2105d31624b0efd
Marcelo Manoel de Lima Filho <marcelo_l_filho@ufms.br>
2024-09-14 22:15:58 -0400

    fix: implement propely method and change the input

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -15,19 +15,14 @@ class GaussSeidel():
         sum = 0
         for j in range(len(self.A)):
             if i != j:
-                print(f"x[{j}] = {self.x[j]}")
-                print(f"A[{i}][{j}] = {self.A[i][j]}")
-                sum  += self.x[j]*self.A[i][j]
-        print(f"b[{i}] = {self.b[i]}")
+                sum  += self.x[j] * self.A[i][j]
         sum = self.b[i] - sum
-        print(round(sum/self.A[i][i], 4))
         return round(sum/self.A[i][i], 4)
 
 
     def iterate(self):
         while self.state:
             self.count += 1
-            print(self.count)
             for i in range(len(self.A)):
                 self.x[i] = self.calculate(i)
             self.state = self.error()
@@ -38,44 +33,46 @@ class GaussSeidel():
         self.y = np.copy(self.x)
 
     def error(self):
-        error = np.round(self.maxNum()/self.maxDem())
+        error = round(self.maxNum()/self.maxDem())
         return error >= self.stop
                 
 
     def maxDem(self):
-        return np.max(np.abs(self.x))
+        return max(abs(self.x))
 
     def maxNum(self):
-        return np.max(np.abs(self.x - self.y))
+        return max(abs(self.x - self.y))
 
 
+#Exemplo prático
 A = np.array([
-    [5, 1, 1],
-    [3, 4, 1],
-    [3, 3 ,6]
+    [17.0, -5.0,  0.0,  0.0,  0.0,  0.0, 0.0,  -2.0],
+    [ 1.0, -4.0,  2.0,  0.0,  0.0,  0.0,  1.0,  0.0],
+    [ 0.0,  6.0, -9.0,  2.0,  0.0,  1.0,  0.0,  0.0],
+    [ 0.0,  0.0,  1.0, -4.0,  3.0,  0.0,  0.0,  0.0],
+    [ 0.0,  0.0,  0.0,  8.0, -9.0,  1.0,  0.0,  0.0],
+    [ 0.0,  0.0,  4.0,  0.0,  3.0, -10.0,  3.0,  0.0],
+    [ 0.0, 12.0,  0.0,  0.0,  0.0,  3.0, -23.0,  8.0],
+    [ 6.0,  0.0,  0.0,  0.0,  0.0,  0.0,  10.0, -31.0]
 ])
 
-b = np.array([5, 6, 0])
+b = np.array([1000.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-x = np.array([0, 0, 0])
+x = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-gauss_solver = GaussSeidel(A,x,b,0.05)
+
+# A = np.array([
+#     [5.0, 1.0, 1.0],
+#     [3.0, 4.0, 1.0],
+#     [3.0, 3.0 ,6.0]
+# ])
+
+# b = np.array([5.0, 6.0, 0.0])
+
+# x = np.array([0.0, 0.0, 0.0])
+
+gauss_solver = GaussSeidel(A,x,b,1e-6)
 gauss_solver.iterate()
 formatted_solution = [f"{xi:.4f}" for xi in gauss_solver.x]
 print(f"vetor solução: {formatted_solution}")
 print(f"numero de iterações: {gauss_solver.count}")
-#Exemplo prático
-# A = np.array([
-#     [17, -5,  0,  0,  0,  0, 0,  -2],
-#     [ 1, -4,  2,  0,  0,  0,  1,  0],
-#     [ 0,  6, -9,  2,  0,  1,  0,  0],
-#     [ 0,  0,  1, -4,  3,  0,  0,  0],
-#     [ 0,  0,  0,  8, -9,  1,  0,  0],
-#     [ 0,  0,  4,  0,  3, -10,  3,  0],
-#     [ 0, 12,  0,  0,  0,  3, -23,  8],
-#     [ 6,  0,  0,  0,  0,  0,  10, -31]
-# ])
-
-# b = np.array([1000, 0, 0, 0, 0, 0, 0, 0])
-
-# x = np.array([0, 0, 0, 0, 0, 0, 0, 0])
